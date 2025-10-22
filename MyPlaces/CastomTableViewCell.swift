5e1ef7b8a699c02ba354587b5754bb10b21025f5
Vladislav Bliznyuk <braacy@yandex.ru>
2025-10-22 20:35:22 +0200

    added custom cell commit

---
+++ MyPlaces/CastomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CastomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Владислав Близнюк on 22.10.2025.
+//
+
+import UIKit
+
+class CastomTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+}
