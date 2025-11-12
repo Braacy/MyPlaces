2d05fa414a4a494c541211703206ed059513941a
Vladislav Bliznyuk <braacy@yandex.ru>
2025-11-12 16:13:28 +0200

    Edit action commit

--- MyPlaces/NewPlaceTableViewController.swift
+++ MyPlaces/NewPlaceTableViewController.swift
@@ -11,6 +11,7 @@ import RealmSwift
 class NewPlaceTableViewController: UITableViewController {
 
     var imageIsChanged = false
+    var currentPlace: Place?
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
     @IBOutlet weak var placeImage: UIImageView!
@@ -22,6 +23,8 @@ class NewPlaceTableViewController: UITableViewController {
         super.viewDidLoad()
         saveButton.isEnabled = false
         placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+        
+        setupEditScreen()
     }
 // MARK: - Table view delegate
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -57,7 +60,7 @@ class NewPlaceTableViewController: UITableViewController {
         }
     }
     
-    func saveNewPlace() {
+    func savePlace() {
         var image: UIImage?
         
         if imageIsChanged {
@@ -72,10 +75,45 @@ class NewPlaceTableViewController: UITableViewController {
                              type: placeType.text,
                              imageData: imageData)
         
-        StorageManager.saveObject(newPlace)
+        if currentPlace != nil {
+            try! realm.write{
+                currentPlace?.name = newPlace.name
+                currentPlace?.location = newPlace.location
+                currentPlace?.type = newPlace.type
+                currentPlace?.imageData = newPlace.imageData
+            }
+        } else {
+            StorageManager.saveObject(newPlace)
+        }
     
     }
 
+    private func setupEditScreen() {
+    if currentPlace != nil {
+        
+        setupNavigationBar()
+        imageIsChanged = true
+        
+        guard let data = currentPlace?.imageData, let image = UIImage(data: data) else {return}
+        placeImage.image = image
+        placeImage.contentMode = .scaleAspectFill
+        
+        placeName.text = currentPlace?.name
+        placeLocation.text = currentPlace?.location
+        placeType.text = currentPlace?.type
+    }
+}
+    
+    private func setupNavigationBar() {
+        if let topItem = navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        }
+        navigationItem.leftBarButtonItem = nil
+        title = currentPlace?.name
+        
+        saveButton.isEnabled = true
+    }
+    
     @IBAction func cancelButtonPressed(_ sender: Any) {
         dismiss(animated: true)
     }
--- MyPlaces/TableViewController.swift
+++ MyPlaces/TableViewController.swift
@@ -63,10 +63,21 @@ class TableViewController: UITableViewController {
     }
     
     //MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showDetail" {
+            guard let indexPath = tableView.indexPathForSelectedRow else {return}
+            let place = places[indexPath.row]
+            
+            let newPlaceVC = segue.destination as! NewPlaceTableViewController
+            newPlaceVC.currentPlace = place
+        }
+    }
+    
     @IBAction func undwindSegue(_ segue: UIStoryboardSegue) {
         guard let newPlaceVC = segue.source as? NewPlaceTableViewController else {return}
         
-        newPlaceVC.saveNewPlace()
+        newPlaceVC.savePlace()
 
         tableView.reloadData()
         
