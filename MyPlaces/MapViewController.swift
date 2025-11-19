2c5066e74875779b8ebf843ab7ce5bf384c7e253
Vladislav Bliznyuk <braacy@yandex.ru>
2025-11-19 17:48:44 +0200

    prepare fo mapKit commit

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Владислав Близнюк on 19.11.2025.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true)
+    }
+    
+}
